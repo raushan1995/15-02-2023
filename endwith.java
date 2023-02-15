a0e20fe823b68b0cda266301d2f48ba4420f0117
Raushan Kumar <priyanshu1996r@gmail.com>
2023-02-15 20:08:43 +0530

    Add files via upload

---
+++ endwith.java
@@ -0,0 +1,6 @@
+public class EndsWithExample{  
+public static void main(String args[]){  
+String s1="welcome to java";  
+System.out.println(s1.endsWith("a"));  
+System.out.println(s1.endsWith("to"));  
+}}  
